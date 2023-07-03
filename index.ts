62bc877f192242f9d28e53073d43387307e6b995
Skaysz <thomas680159753@gmail.com>
2023-07-03 13:31:18 -0300

    Add

--- index.ts
+++ index.ts
@@ -1,29 +1,56 @@
+import * as fs from "fs";
+import * as pullAllBy from "loadash/pullAllBy";
 class ListaDeCosas {
-  name: string;
-  cosas: any[] = [];
-  constructor(name: string) {
-    // nombre de esta lista
-    this.name = name;
-  }
-  add(nuevaCosa) {
-    this.cosas.push(nuevaCosa);
-  }
-  getCosas() {
-    return this.cosas;
-  }
+	name: string;
+	cosas: any[] = [];
+	constructor(name: string) {
+		// nombre de esta lista
+		this.name = name;
+	}
+	add(nuevaCosa) {
+		this.cosas.push(nuevaCosa);
+	}
+	getCosas() {
+		return this.cosas;
+	}
 }
 
 class Product {
-  name: string;
-  price: number;
-  id: number;
-  constructor(name: string, price: number, id: number) {
-    this.name = name;
-    this.price = price;
-    this.id = id;
-  }
+	name: string;
+	price: number;
+	id: number;
+	constructor(name: string, price: number, id: number) {
+		this.name = name;
+		this.price = price;
+		this.id = id;
+	}
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+	constructor(name: string) {
+		super(name);
+
+		const contenidoDelArchivo = fs
+			.readFileSync(__dirname + "/products.js")
+			.toString();
+		const productosDelArchivo = JSON.parse(contenidoDelArchivo);
+
+		productosDelArchivo.forEach((p) => {
+			this.addProduct(p);
+		});
+	}
+
+	addProduct(product: Product) {
+		this.add(product);
+	}
+	getProduct(id: number): Product {
+		const cosas = this.getCosas();
+		return cosas.find((c) => c.id == id);
+	}
+	removeProduct(a) {
+		return false;
+	}
+	getSortedByPrice() {}
+}
 
 export { ListaDeProductos, Product };
